3f157870fed49183dde9ca99230919d775b30e04
Sergey Kim <Sergeykim486@gmail.com>
2022-11-26 23:25:24 +0500

    Task 4

---
+++ Task4.py
@@ -0,0 +1,9 @@
+n = int(input(f'Укажите номер четверти (1-4) ->  '))
+if n == 1:
+    print('x>0, y>0')
+elif n == 2:
+    print('x<0, y>0')
+elif n == 3:
+    print('x<0, y<0')
+elif n == 4:
+    print('x>0, y<0')
