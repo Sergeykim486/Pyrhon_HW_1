0bc272b7de6c5844ac5b41656826bd36841c3395
Sergey Kim <Sergeykim486@gmail.com>
2022-11-26 23:09:20 +0500

    Task 1 2

---
+++ Task2.py
@@ -0,0 +1,10 @@
+x = int(input(f'Введите значение "x" ->  '))
+y = int(input(f'Введите значение "y" ->  '))
+z = int(input(f'Введите значение "z" ->  '))
+first = not(x or y or z)
+second = not x and not y and not z
+result = first == second
+if result == True:
+    print('Утверждение истинно')
+else:
+    print('Утверждение ложно')
